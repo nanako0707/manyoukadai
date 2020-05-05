64c77849f8146193fc8b402fbe10e5df3b0513d9
kan nanako <n.k.0213.0529@gmail.com>
2020-05-05 14:51:51 +0900

    タスク一覧画面の作成

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ module Manyoukadai
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    I18n.enforce_available_locales = false
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.generators do |g|
